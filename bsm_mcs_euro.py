560893bdd6223de2b718b1c2e6718c4f9b26c5c8
qinkevin <1170549742@qq.com>
2016-12-21 17:33:35 +0800

    第三章

--- bsm_mcs_euro.py
+++ bsm_mcs_euro.py
@@ -1,15 +1,19 @@
 import numpy as np
+import matplotlib.pyplot as plt
 
 S0 = 100
 K = 105
 T = 1.0
 r = 0.05
-sigma = 0.2
+sigma = 0.2/250
 
 I = 100000
 
 z = np.random.standard_normal(I)
 ST = S0 * np.exp((r-0.5*sigma**2)*T+sigma*np.sqrt(T)*z)
+
+plt.plot(ST)
+
 
 hT = np.maximum(ST-K, 0)
 C0 = np.exp(-r * T)*np.sum(hT) / I
